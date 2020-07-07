dc8cd50a2c65ddb4b069b10ff20bcba64837197c
Maximo <38792883+MaxCorpIndustries@users.noreply.github.com>
2020-07-07 15:59:52 -0400

    Testing with third person camera systems

---
+++ SarkTank- TheVideoGame (collab version)/Assets/Snark Tank/Soldier Character/ThirdPersonCharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCharacterController : MonoBehaviour
+{
+	
+	public float Speed; //this will determine character speed
+
+    // Update is called once per frame
+    void Update()
+    {
+		PlayerMovement(); //call PlayerMovement in update
+    }
+	
+	void PlayerMovement()
+	{
+		//assign movement positions to float variables
+		float hor = -Input.GetAxis("Horizontal");
+		float ver = -Input.GetAxis("Vertical");
+		
+		//implement input into moving player
+		Vector3 PlayerMovement = new Vector3(hor,0f,ver) * Speed * Time.deltaTime;
+		
+		//actualling moving the player
+		transform.Translate(PlayerMovement, Space.Self);
+
+	if (Input.GetKeyDown(KeyCode.LeftAlt))
+	{
+		print("alt key was pressed");
+	}
+
+	}
+}
